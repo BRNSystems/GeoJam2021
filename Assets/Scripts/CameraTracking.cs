4ef1ee9f97939529002c16bbe0a3f274e16a5590
Tucan444 <ben4442004@gmail.com>
2021-07-22 14:52:39 +0200

    _

--- Assets/Scripts/CameraTracking.cs
+++ Assets/Scripts/CameraTracking.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class CameraTracking : MonoBehaviour
 {
     public Transform player;
+    [Range(0.1f, 2)] [SerializeField] float smoothing = 0.6f;
     // Start is called before the first frame update
+
+    Vector3 player_pos;
+    Vector3 camera_pos;
+    Vector2 difference = new Vector2(0, 0);
+
     void Start()
     {
-        
+        camera_pos = transform.position;
+
+        camera_pos[2] = -10;
+
+        transform.position = camera_pos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 playpos = player.transform.position;
-        playpos.z = playpos.z - 10;
-        playpos.y = playpos.y + 3;
-        transform.position = playpos;
+        player_pos = player.transform.position;
+        camera_pos = transform.position;
+
+        difference[0] = player_pos[0] - camera_pos[0];
+        difference[1] = player_pos[1] - camera_pos[1];
+
+        float smoothing_ = smoothing / Time.deltaTime;
+
+        camera_pos[0] += difference[0] / smoothing_;
+        camera_pos[1] += difference[1] / smoothing_;
+
+        transform.position = camera_pos;
     }
 }
