4d0551d339b321d9209953a616c51421d82057f8
Tucan444 <ben4442004@gmail.com>
2021-07-23 17:00:30 +0200

    _

--- Assets/Scripts/MenuPlay.cs
+++ Assets/Scripts/MenuPlay.cs
@@ -8,6 +8,9 @@ public class MenuPlay : MonoBehaviour
     public LayerMask contactFilter;
     public AudioSource sfx;
     public MenuMouse mouse;
+    public SpriteRenderer sr;
+    private bool hover = false;
+    private bool previous_hover = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +20,24 @@ public class MenuPlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
-            if(Physics2D.CircleCast(mouse.pos, 1f, Vector2.zero, 0f, contactFilter)){
+        if(Physics2D.CircleCast(mouse.pos, 1f, Vector2.zero, 0f, contactFilter)){
+            if (Input.GetMouseButtonDown(0)){
                 sfx.Play();
                 SceneManager.LoadScene("Tutorial");
+            }
+
+            previous_hover = hover;
+            hover = true;
+        } else {
+            previous_hover = hover;
+            hover = false;
+        } 
+
+        if (hover != previous_hover) {
+            if(hover) {
+                sr.color = new Color(202f / 255f, 202f / 255f, 242f / 255f, 201f / 255f);
+            } else {
+                sr.color = new Color(149f / 255f, 189f / 255f, 253f / 255f, 201f / 255f);
             }
         }
     }
